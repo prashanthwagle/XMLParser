91929092e4bcbfc58a9f7f5607e2f046ed02e0d5
Prashanth <prashanthwagle360@gmail.com>
2024-01-17 13:43:29 -0500

    classified the functions

--- xml_parser.py
+++ xml_parser.py
@@ -7,24 +7,55 @@ class TreeNode:
         self.parent = None
 
 
+class ParseTree:
+    def __init__(self, xml_string):
+        self.stack = []
+        self.root_node = None
+        self.curr_node = None
+        self.xml_string = xml_string
+
+    def build_tree(self):
+        current_tag = ""
+        for char in self.xml_string:
+            if char == '<':
+                current_tag = ""
+            elif char == '>':
+                self._handle_tag(current_tag)
+            else:
+                current_tag += char
+
+    def _handle_tag(self, tag):
         if tag.startswith('/'):
-            handle_closing_tag(tag[1:])
+            self._handle_closing_tag(tag[1:])
         elif len(tag) > 3 and tag[-2] == "/":
-            handle_self_closing_tag(tag[1:-2])
+            self._handle_self_closing_tag(tag[1:-2])
         else:
-            handle_opening_tag(tag)
+            self._handle_opening_tag(tag)
 
-    def handle_opening_tag(tag):
-        stack.append(tag)
-        print("Opening tag", stack)
+    def _handle_opening_tag(self, tag):
+        if self.root_node == None:
+            self.root_node = TreeNode(tag)
+            self.curr_node = self.root_node
+        else:
+            newNode = TreeNode(tag)
+            self.curr_node.children.append(newNode)
+            newNode.parent = self.curr_node
+            self.curr_node = newNode
+        self.stack.append(tag)
+        print("Opening tag", self._stack_dump())
 
-    def handle_self_closing_tag(tag):
-        print("Self Closing tag", stack)
+    def _handle_self_closing_tag(self, tag):
+        print("Self Closing tag", self._stack_dump())
+        newNode = TreeNode(tag)
+        self.curr_node.children.append(newNode)
+        newNode.parent = self.curr_node
+        self.curr_node = newNode
 
-    def handle_closing_tag(tag):
-        if stack and stack[-1] == tag:
-            print("Closing tag", stack)
-            stack.pop()
+    def _handle_closing_tag(self, tag):
+        self.curr_node = self.curr_node.parent
+        if self.stack and self.stack[-1] == tag:
+            print("Closing tag", self._stack_dump())
+            self.stack.pop()
         else:
             print(f"Error: Mismatched closing tag '{tag}'")
 

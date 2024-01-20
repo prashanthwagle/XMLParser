6abc1f0fe7015d648cacafb5c371a2f12c84933b
Prashanth <prashanthwagle360@gmail.com>
2024-01-20 00:31:45 -0500

    added logic to handle errors/warnings

--- xml_parser.py
+++ xml_parser.py
@@ -28,10 +28,12 @@ class ParseTree:
         self.stack = []
         self.root_node = None
         self.curr_node = None
-        self.xml_string = self.__remove_xml_declaration(xml_string)
         self.xml_data = []
         self.metadata = {"title": None, "description": None,
                          "author": None, "creationDate": None}
+        self.is_error = False
+        self.is_warning = False
+        self.return_message = ""
 
     def __standardize_json(self, ip_dict):
         """
@@ -58,8 +60,24 @@ class ParseTree:
         """
         return True if 'header' in self.stack or len(self.stack) == 1 else False
 
-    def __remove_xml_declaration(self, xml_string):
-        return re.sub(r'<?xml .+ ?>', '', xml_string)
+    def __remove_xml_declaration(self, old_xml):
+        new_xml = re.sub(REGEX_XML_DEC, '', old_xml)
+        if new_xml == old_xml:
+            self.__add_warning(MSG_WARN_NO_DEC)
+        return new_xml
+
+    # Interesting: It is not able to access __REGEX_VLD_XML_TG but able to access REGEX_VLD_XML_TG
+    def __is_valid_xml_tag(self, tag):
+        if not bool(REGEX_VLD_XML_TG.match(tag)):
+            self.__add_warning(MSG_WARN_INVLD_TAG.format(tag))
+
+    def __add_warning(self, message):
+        self.is_warning = self.is_warning or True
+        self.return_message += message
+
+    def __add_error(self, message):
+        self.is_error = self.is_error or True
+        self.return_message += message
 
     def build_tree(self):
         current_tag = ""
@@ -69,10 +87,12 @@ class ParseTree:
         for char in self.xml_string:
             if char == '<':
                 if capturing_metadata:
-                    # If the metadata tags is nested in the header tag, only then consider it as metadata
                     capturing_metadata = False
-                    # TODO: ErrorCheck if this field has already been filled (duplicate metadata)
-                    self.metadata[current_tag] = metadata_contents
+                    if self.metadata[current_tag]:
+                        self._add_warning(
+                            MSG_WARN_DUP_METADATA.format(current_tag))
+                    else:
+                        self.metadata[current_tag] = metadata_contents
                     metadata_contents = ""
                 current_tag = ""
                 # TODO: ErrorCheck: What if metadata is already filled?
@@ -97,14 +117,23 @@ class ParseTree:
         if tag.startswith('!') and bool(re.search(r'!--(.*?)--', tag, re.DOTALL)):
             return
 
+        # Discard all comments
+        if tag.startswith('!') and bool(re.search(REGEX_XML_COMMS, tag, re.DOTALL)):
+            self.__add_warning(MSG_WARN_REMOVED_COMMS)
+            return
+        # Closing Tag
         if tag.startswith('/'):
             self._handle_closing_tag(self.__clean_tags(tag[1:]))
+        # Self Closing Tag
         elif len(tag) > 3 and tag[-1] == "/":
             self._handle_self_closing_tag(self.__clean_tags(tag[:-1]))
+        # Opening Tag
         else:
             self._handle_opening_tag(self.__clean_tags(tag))
 
     def _handle_opening_tag(self, tag):
+        self.__is_valid_xml_tag(tag)
+
         if self.root_node == None:
             self.root_node = TreeNode(tag)
             self.curr_node = self.root_node
@@ -114,10 +143,12 @@ class ParseTree:
             new_node.parent = self.curr_node
             self.curr_node = new_node
         self.stack.append(tag)
-        print("Opening tag", self._stack_dump())
+        # print("Opening tag", self._stack_dump())
 
     def _handle_self_closing_tag(self, tag):
-        print("Self Closing tag", self._stack_dump())
+        self.__is_valid_xml_tag(tag)
+
+        # print("Self Closing tag", self._stack_dump())
         new_node = TreeNode(tag)
         new_node.is_self_closing = True
         self.curr_node.children.append(new_node)
@@ -125,13 +156,18 @@ class ParseTree:
         self.curr_node = new_node
 
     def _handle_closing_tag(self, tag):
-        # TODO: ErrorCheck if this field has already been filled (duplicate metadata)
+        self.__is_valid_xml_tag(tag)
+
         self.curr_node = self.curr_node.parent
+        if not self.stack:
+            self.__add_error(MSG_ERR_INVALID_XML)
+            return
         if self.stack and self.stack[-1] == tag:
-            print("Closing tag", self._stack_dump())
+            # print("Closing tag", self._stack_dump())
             self.stack.pop()
         else:
-            print(f"Error: Mismatched closing tag '{tag}'")
+            self.__add_error(MSG_ERR_MATCH_TAGS.format(self.stack[-1]))
+            return
 
     def _stack_dump(self):
         return self.stack

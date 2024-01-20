8b42a9e851d7a0c0d213f04db11922bfce5dd81f
Prashanth <prashanthwagle360@gmail.com>
2024-01-20 15:36:35 -0500

    added code to install dependencies on multiple platforms

--- run_app.py
+++ run_app.py
@@ -1,4 +1,20 @@
-from app import app
+import subprocess
+import sys
 
+try:
+    import flask
+except ImportError:
+    pip_command = "pip3" if sys.platform.startswith(
+        "linux") or sys.platform.startswith("darwin") else "pip"
+    subprocess.run([pip_command, "install", "Flask"])
+
+try:
+    import unittest
+except ImportError:
+    pip_command = "pip3" if sys.platform.startswith(
+        "linux") or sys.platform.startswith("darwin") else "pip"
+    subprocess.run([pip_command, "install", "unittest2"])
+
+from app import app
 if __name__ == "__main__":
     app.run(port=3456)
